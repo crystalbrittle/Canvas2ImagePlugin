cf9a42f25e33cdf6d925ba7a7ac3ef3db67e97dc
crystalbrittle <m@bludgeonsoft.org>
2019-07-26 20:22:09 -0700

    dataFromBase64String replaced with newer initWithBase64EncodedString

--- src/ios/Canvas2ImagePlugin.m
+++ src/ios/Canvas2ImagePlugin.m
@@ -4,7 +4,7 @@
 //
 //  Created by Tommy-Carlos Williams on 29/03/12.
 //  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
-//	MIT Licensed
+//  MIT Licensed
 //
 
 #import "Canvas2ImagePlugin.h"
@@ -22,11 +22,15 @@
 - (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
 {
     self.callbackId = command.callbackId;
-	NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0] options:0];
-	
-	UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];	
-	UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-	
+    /// OLD WAY, DEPRECATED
+    ///NSData* imageData = [NSData dataFromBase64String:[command.arguments objectAtIndex:0]];
+    // == == == == == NEW WAY MARK
+    NSString* objectAtIndex0 = [command argumentAtIndex:0];
+    NSData* imageData = [[NSData alloc] initWithBase64EncodedString:objectAtIndex0 options:0];
+  //
+  UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];  
+  UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+  
 }
 
 - (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo
@@ -36,21 +40,21 @@
     {
         // Show error message...
         NSLog(@"ERROR: %@",error);
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
-		[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:error.description];
+    [self.webView stringByEvaluatingJavaScriptFromString:[result toErrorCallbackString: self.callbackId]];
     }
     else  // No errors
     {
         // Show message image successfully saved
         NSLog(@"IMAGE SAVED!");
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
-		[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+    CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString:@"Image saved"];
+    [self.webView stringByEvaluatingJavaScriptFromString:[result toSuccessCallbackString: self.callbackId]];
     }
 }
 
 - (void)dealloc
-{	
-	[callbackId release];
+{  
+  [callbackId release];
     [super dealloc];
 }
 
